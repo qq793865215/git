af21c6fb8d7aa0487aea2d697286484fbc7ac4f9
郭银嘉 <https://github.com/GuoYinJia/oa.git>
2018-05-28 17:16:10 +0800

    山东省vs的V

---
+++ src/Gyj.java
@@ -0,0 +1,10 @@
+/**
+ * @author ：郭银嘉
+ * @create ： 2018/05/28 17:15
+ * @description ：
+ */
+public class Gyj{
+    public static void main(String[] args) {
+        System.out.println("22222");
+    }
+}
